a691c1fc654fc428edfcfc91c7d78e0b5e32b414
GUDUNA <gudaguduna@gmail.com>
2024-05-18 21:56:27 -0400

    initial and possibly last commit
    7 bugs corrected one new bug appeared
    after filtering by employee and returning
    back to all employee 'view more' button disappears

---
+++ src/components/InputCheckbox/index.tsx
@@ -0,0 +1,28 @@
+import classNames from "classnames"
+import { useRef } from "react"
+import { InputCheckboxComponent } from "./types"
+
+export const InputCheckbox: InputCheckboxComponent = ({ id, checked = false, disabled, onChange }) => {
+  const { current: inputId } = useRef(`RampInputCheckbox-${id}`)
+  const letsHandle = () => {
+    onChange(!checked)
+  }
+
+  return (
+    <div className="RampInputCheckbox--container" data-testid={inputId} onClick={letsHandle}>
+      <label
+        className={classNames("RampInputCheckbox--label", {
+          "RampInputCheckbox--label-checked": checked,
+          "RampInputCheckbox--label-disabled": disabled,
+        })}
+      />
+      <input
+        id={inputId}
+        type="checkbox"
+        className="RampInputCheckbox--input"
+        defaultChecked={checked}
+        disabled={disabled}
+        />
+    </div>
+  )
+}
